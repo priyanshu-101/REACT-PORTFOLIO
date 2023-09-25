a5509889160ffe4cfb3e59600e575cdd1408dbd4
Priyanshu <priyanshuagarwal1008@gmail.com>
2023-09-25 22:32:16 +0530

    Make contact me page

--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -29,6 +29,20 @@ function Navigation() {
         
         </Container>
 
+        <Container>
+        <Navbar.Brand href="/" className="d-flex">
+          <img src={"https://t4.ftcdn.net/jpg/05/25/08/09/360_F_525080936_JEpnKXh2siYKBPpsqd98pbbcIzy4ySKz.jpg"} className="img-fluid logo" alt="brand" />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+        </Navbar.Toggle>
+        
+        </Container>
+
     </Navbar>
   );
 }
